1f2d436a73a89c01eb3ddfbb24c90b29cdfffa85
yogeshkumawat2027 <yogeshkumawat2027@gmail.com>
2026-06-21 13:19:19 +0530

    built auth controller

---
+++ backend/controllers/authController.js
@@ -0,0 +1,96 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import User from "../models/User.js";
+import asyncHandler from "../middleware/asyncHandler.js";
+
+export const registerUser = asyncHandler(async(req , res)=>{
+
+     const { name, email, password, role } = req.body;
+
+     if (!name || !email || !password){
+        return res.status(400).json({ success : false , message : "all fileds are required"});
+     }
+
+     const existingUser = await User.findOne({ email });
+
+     if(existingUser) return res.status(400).json({ success : false , message : "User already exist"});
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+        name,
+        email,
+        password : hashedPassword,
+        role 
+    })
+
+      const token = jwt.sign(
+           { id: user._id,role: user.role},
+           process.env.JWT_SECRET,{expiresIn: "7d"}
+     );
+        
+
+    res.cookie("token", token, {
+        httpOnly: true,
+        maxAge: 7 * 24 * 60 * 60 * 1000
+    });
+
+    res.status(201).json({success  : true , message : "Registration successful" , user});
+
+})
+
+export const loginUser = asyncHandler( async(req, res) =>{
+
+  const { email, password } = req.body;
+
+  if (!email || !password){
+    return res.status(400).json({success: false,message: "Email and password are required"}); 
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user){
+    return res.status(404).json({success: false,message: "user not found"});
+  }
+
+  const isMatch = await bcrypt.compare(password,user.password);
+    
+    
+  
+
+  if (!isMatch) {
+    return res.status(401).json({success: false, message: "Invalid credentials"});
+    
+  }
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+      role: user.role
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "7d"
+    }
+  );
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    maxAge: 7 * 24 * 60 * 60 * 1000
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Login successful",
+    user
+  });
+
+});
+
+export const logoutUser = asyncHandler( async (req, res)=>{
+
+  res.clearCookie("token");
+
+  res.status(200).json({success: true,message: "Logout successful" });
+
+});
