6b77e79649e9fe44314ad2613977e398e5044fef
yogeshkumawat2027 <yogeshkumawat2027@gmail.com>
2026-06-22 11:53:32 +0530

    update auth controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -79,11 +79,8 @@ export const loginUser = asyncHandler( async(req, res) =>{
     maxAge: 7 * 24 * 60 * 60 * 1000
   });
 
-  res.status(200).json({
-    success: true,
-    message: "Login successful",
-    user
-  });
+  res.status(200).json({success: true,user});
+    
 
 });
 
