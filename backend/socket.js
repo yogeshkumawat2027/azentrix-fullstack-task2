e22e195a8064f6fadac5d6437c4749d765c21d5e
yogeshkumawat2027 <yogeshkumawat2027@gmail.com>
2026-06-25 10:28:07 +0530

    socket setup to real time communication

---
+++ backend/socket.js
@@ -0,0 +1,40 @@
+import { Server } from "socket.io";
+
+let io;
+
+export const initSocket = (server) => {
+  io = new Server(server, {
+    cors: {
+      origin: "http://localhost:5173",
+      credentials: true,
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log("Socket connected:", socket.id);
+
+    socket.on("join-board", (boardId) => {
+      socket.join(boardId);
+      console.log(`Socket ${socket.id} joined board ${boardId}`);
+    });
+
+    socket.on("leave-board", (boardId) => {
+      socket.leave(boardId);
+      console.log(`Socket ${socket.id} left board ${boardId}`);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Socket disconnected:", socket.id);
+    });
+  });
+
+  return io;
+};
+
+export const getIO = () => {
+  if (!io) {
+    throw new Error("Socket.io not initialized");
+  }
+
+  return io;
+};
